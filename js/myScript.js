e9470be6c2a0a3e0399ff83328d7dbbee852eaac
Oleshko <olehchoma@gmail.com>
2021-11-17 10:56:44 +0200

    Create

---
+++ js/myScript.js
@@ -0,0 +1,10 @@
+var click = 0;
+$("#nav-btn").click(function () {
+  if (click%2==0) {
+    $("#header").addClass("nav-clicked");
+  }
+  else {
+    $("#header").removeClass("nav-clicked");
+  }
+  click++;
+});
