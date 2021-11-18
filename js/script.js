08e1f6cd9c706a8e9a615be089c9ab2021ff72cb
Oleshko <olehchoma@gmail.com>
2021-11-18 16:21:37 +0200

    Animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,58 @@
-let click = 0;
+// ! Navbar
+var click = 0;
 $("#nav-btn").click(function () {
-  if (click%2==0) {
+  if (click % 2 == 0) {
     $("#header").addClass("nav-clicked");
-  }
-  else {
+  } else {
     $("#header").removeClass("nav-clicked");
   }
   click++;
+});
+
+// ! Arrows
+$(function() {
+
+  setTimeout(function() {
+
+    setInterval(function() {
+      // Move left
+      $('#btn-left').removeClass('btn-outline-dark').addClass('btn-dark');
+      $('#btn-right').addClass('btn-outline-dark').removeClass('btn-dark');
+
+      setTimeout(function() {
+        // Move right
+        $('#btn-left').addClass('btn-outline-dark').removeClass('btn-dark');
+        $('#btn-right').removeClass('btn-outline-dark').addClass('btn-dark');
+
+        setTimeout(function() {
+          // Move left
+          $('#btn-left').removeClass('btn-outline-dark').addClass('btn-dark');
+          $('#btn-right').addClass('btn-outline-dark').removeClass('btn-dark');
+        }, 4000)
+
+      }, 2000)
+
+
+    }, 8000);
+
+  }, 0);
+
+});
+
+// ! Space
+$(function() {
+
+  setTimeout(function() {
+
+    setInterval(function() {
+      $('#btn-space').removeClass('btn-outline-dark').addClass('btn-dark');
+
+      setTimeout(function() {
+        $('#btn-space').addClass('btn-outline-dark').removeClass('btn-dark');
+      }, 500);
+
+    }, 2000);
+
+  }, 1700);
+
 });
